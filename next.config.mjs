abfcc1b59062782bf99c76e7ccd93a3b5352e158
AmirSeraj <amir.seraj6990@gmail.com>
2024-05-13 00:34:14 +0330

    folder structure

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: [process.env.NEXT_PUBLIC_APP_HOSTNAME],
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: process.env.NEXT_PUBLIC_APP_HOSTNAME || "192.168.88.253",
+          pathname: "**",
+          port: "",
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
